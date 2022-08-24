f016a18d90fdb3cf27b35612ec8173f2337cce67
rosi <rosiyanto.syafrudin@mekari.com>
2022-08-24 16:37:17 +0700

    add html element

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,8 @@ class PagesController < ApplicationController
     @language = ['ruby', 'ruby on rails', 'HTML fundamental', 'node.js']
     @title = 'My Programming language'
   end
-  def halo
-    @halo = 'Hello everyone, My name is Mark Prima nabil akbar'
+  def hello
+    @hello = 'Hello everyone, My name is Mark Prima nabil akbar'
     @skill = ['ruby', 'ruby on rails', 'HTML fundamental', 'node.js']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/home', to: 'pages#home'
-  get '/halo', to: 'pages#halo'
+  get '/hello', to: 'pages#hello'
 end
